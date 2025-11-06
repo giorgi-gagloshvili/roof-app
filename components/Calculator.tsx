21db0eda7fb6359fb096a1c46d06c9275ec4e1fa
giorgi-gagloshvili <g.gagloshvili@gmail.com>
2025-11-06 19:51:37 +0400

    calc fixed

--- components/Calculator.tsx
+++ components/Calculator.tsx
@@ -23,32 +23,50 @@ const materials = [
   },
 ];
 
-const GENERAL_PRICE = 30;
+const services = [
+  {
+    id: 1,
+    name: "კონსტრუქციის აწყობა/გადახურვა",
+    price: 50,
+  },
+  {
+    id: 2,
+    name: "გადახურვა",
+    price: 30,
+  },
+];
 
 const Calculator = () => {
-  const [material, setmaterial] = useState<null | string>(null);
-  const [area, setArea] = useState<string | number>(0);
+  const [initialPrice, setInitialPrice] = useState<string | number>(0);
+  const [material, setMaterial] = useState<null | string>(null);
+  const [area, setArea] = useState<string | number>("");
   const [totalPrice, setTotalPrice] = useState<null | number>(null);
   const [materialPrice, setMaterialPrice] = useState<null | number>(null);
 
   useEffect(() => {
-    setTotalPrice(Number(area) * GENERAL_PRICE);
+    setTotalPrice(Number(area) * Number(initialPrice));
     const finded = materials.find(
       (item) => item.id === Number(material)
     )?.price;
+
+    console.log(finded);
+
     if (finded) {
       setMaterialPrice(Number(area) * finded);
     }
-  }, [area, material]);
+  }, [area, material, initialPrice]);
 
   return (
     <div className="flex flex-col items-center py-12 lg:py-20 max-w-[1312px] mx-auto px-4">
       <Heading>ფასის კალკულაცია</Heading>
       <Tagline>შეგახსენებთ რომ კალკულატორი ითვლის დაახლოებით ფასს</Tagline>
 
-      <div className="p-6 border border-slate-300 rounded-xl w-full bg-white mt-8">
-        <div className="flex gap-x-4 w-full">
-          <div className="w-full">
+      <div className="p-4 md:p-6 border border-slate-300 rounded-xl w-full bg-white mt-8">
+        <p className="border border-blue-600/75 p-3 rounded-lg bg-blue-600/10">
+          ხელობის ფასის დასათვლელად შეიყვანეთ ფართობი და მომსახურების ტიპი
+        </p>
+        <div className="flex flex-col md:flex-row gap-x-4 gap-y-2 w-full mt-2">
+          <div className="grow">
             <input
               type="text"
               className="border border-gray-300 rounded-md w-full px-4 py-2.5"
@@ -57,7 +75,7 @@ const Calculator = () => {
               onChange={(e) => setArea(e.target.value)}
             />
           </div>
-          <div className="w-full">
+          <div className="grow">
             {/* <Select.Root value={value} onValueChange={setValue}>
             <Select.Trigger className="border border-slate-300 rounded-md px-4 py-2 relative">
               <Select.Value aria-label={value}>{value}</Select.Value>
@@ -80,7 +98,20 @@ const Calculator = () => {
           </Select.Root> */}
             <select
               className="border border-gray-300 px-4 rounded-md py-2.5 w-full outline-none"
-              onChange={(e) => setmaterial(e.target.value)}
+              onChange={(e) => setInitialPrice(e.target.value)}
+            >
+              <option value="">მომსახურების არჩევა</option>
+              {services.map((item) => (
+                <option key={item.id} value={item.price}>
+                  {item.name}
+                </option>
+              ))}
+            </select>
+          </div>
+          <div className="grow">
+            <select
+              className="border border-gray-300 px-4 rounded-md py-2.5 w-full outline-none"
+              onChange={(e) => setMaterial(e.target.value)}
             >
               <option value="">საშენი მასალის არჩევა</option>
               {materials.map((item) => (
@@ -95,19 +126,29 @@ const Calculator = () => {
         <div className="mt-8">
           <div className="flex justify-between">
             <p className="font-tbc-medium">ხელობის ფასი</p>{" "}
-            <span className="text-lg">{totalPrice?.toFixed(2)} ლარი</span>
+            <span className="text-lg">
+              {totalPrice && initialPrice ? totalPrice?.toFixed(2) : "0.00"}{" "}
+              ლარი
+            </span>
           </div>
-          <div className="flex justify-between mt-4">
-            <p className="font-tbc-medium">მასალის ფასი</p>{" "}
-            {materialPrice && (
-              <span className="text-lg">{materialPrice?.toFixed(2)} ლარი</span>
-            )}
-          </div>
-          {totalPrice && materialPrice ? (
+          {materialPrice ? (
             <div className="flex justify-between mt-4">
+              <p className="font-tbc-medium">მასალის ფასი</p>{" "}
+              <span className="text-lg">{materialPrice?.toFixed(2)} ლარი</span>
+            </div>
+          ) : null}
+          {totalPrice || materialPrice ? (
+            <div className="flex justify-between mt-4 border-t border-gray-300 pt-4">
               <p className="font-tbc-medium">ჯამური ფასი</p>{" "}
               <span className="text-lg">
-                {(totalPrice + materialPrice).toFixed(2)} ლარი
+                {totalPrice && materialPrice
+                  ? (totalPrice + materialPrice).toFixed(2)
+                  : totalPrice && !materialPrice
+                  ? totalPrice.toFixed(2)
+                  : materialPrice && !totalPrice
+                  ? materialPrice.toFixed(2)
+                  : "0.00"}{" "}
+                ლარი
               </span>
             </div>
           ) : null}
